2d7963a090acfacb8f73da28c9f561d2f16c7ad9
hyunha95 <nohyunha95@gmail.com>
2022-10-17 12:58:02 +0900

    jdbcTemplate

--- spring_in_action/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ spring_in_action/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -7,12 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
+import tacos.Order;
 import tacos.Taco;
 import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -23,13 +23,27 @@ import java.util.stream.Collectors;
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
 	private final IngredientRepository ingredientRepo;
 
+	private TacoRepository tacoRepo;
+
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepo) {
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
 		this.ingredientRepo = ingredientRepo;
+		this.tacoRepo = tacoRepo;
+	}
+
+	@ModelAttribute(name = "order")
+	public Order order() {
+		return new Order();
+	}
+
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
 	}
 
 	@GetMapping
@@ -67,12 +81,14 @@ public class DesignTacoController {
 	}
 
 	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors) {
+	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
 		if(errors.hasErrors()) {
 			return "design";
 		}
+//		log.info("Processing design: " + design);
 
-		log.info("Processing design: " + design);
+		Taco saved = tacoRepo.save(design);
+		order.addDesign(saved);
 
 		return "redirect:/orders/current";
 	}
