717f1c06bebfecb6df8437b7e412ea760b128549
nohyunha95 <nohyunha95@gmail.com>
2022-10-17 23:45:21 +0900

    jpa 페이지 이동 처리

--- spring_boot_web_project/guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ spring_boot_web_project/guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -3,6 +3,7 @@ package org.zerock.guestbook.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.dto.PageResultDTO;
@@ -41,6 +42,30 @@ public class GuestbookServiceTests {
             System.out.println(guestbookDTO);
         }
         System.out.println("==============================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+
+    @Test
+    public void testSearch() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc") // 검색 조건 t, c, w, tc, tcw ...
+                .keyword("한글") // 검색 키워드
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("--------------------------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("=================================================");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 }
