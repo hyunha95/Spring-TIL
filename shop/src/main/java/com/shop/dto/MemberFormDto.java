2cc2a4625203aa8176c1b0c4883cf6e9fad8dba8
nohyunha95 <nohyunha95@gmail.com>
2022-11-02 22:46:50 +0900

    스프링 시큐리티 로그인 테스트 코드 작성

--- shop/src/main/java/com/shop/dto/MemberFormDto.java
+++ shop/src/main/java/com/shop/dto/MemberFormDto.java
@@ -1,6 +1,8 @@
 package com.shop.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
@@ -10,6 +12,7 @@ import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class MemberFormDto {
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
@@ -25,4 +28,12 @@ public class MemberFormDto {
 
     @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
+
+    @Builder
+    public MemberFormDto(String name, String email, String password, String address) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+    }
 }
