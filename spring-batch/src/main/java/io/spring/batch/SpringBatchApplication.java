7daf52047aa8835bb1c557e7e7440f722b4c98fd
nohyunha95 <nohyunha95@gmail.com>
2023-03-12 20:38:59 +0900

    spring-batch

---
+++ spring-batch/src/main/java/io/spring/batch/SpringBatchApplication.java
@@ -0,0 +1,91 @@
+package io.spring.batch;
+
+import io.spring.batch.common.DailyJobTimestamper;
+import io.spring.batch.common.JobLoggerListener;
+import io.spring.batch.common.ParameterValidator;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.listener.JobListenerFactoryBean;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class SpringBatchApplication {
+
+//    @Autowired
+//    private JobBuilderFactory jobBuilderFactory;
+//
+//    @Autowired
+//    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public CompositeJobParametersValidator validator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+        DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
+                new String[] {"fileName"},
+                new String[] {"name", "currentDate"}
+        );
+
+        defaultJobParametersValidator.afterPropertiesSet();
+
+        validator.setValidators(
+                Arrays.asList(new ParameterValidator(), defaultJobParametersValidator)
+        );
+
+        return validator;
+    }
+
+//    @Bean
+//    public Job job() {
+//        return this.jobBuilderFactory.get("basicJob")
+//                .start(step1())
+//                .validator(validator())
+//                .incrementer(new DailyJobTimestamper())
+//                .listener(JobListenerFactoryBean.getListener(
+//                        new JobLoggerListener()
+//                ))
+//                .build();
+//    }
+
+//    @Bean
+//    public Step step1() {
+//        return this.stepBuilderFactory.get("step1")
+//                .tasklet(helloWorldTasklet(null, null))
+//                .build();
+//    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(@Value("#{jobParameters['name']}") String name, @Value("#{jobParameters['fileName']}") String fileName) {
+        return (stepContribution, chunkContext) -> {
+//            String name = (String) chunkContext.getStepContext()
+//                    .getJobParameters()
+//                    .get("name");
+
+            System.out.println(String.format("Hello, %s!", name));
+            System.out.println(String.format("fileName = %s", fileName));
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+
+}
