134c6452651370f18e5bea22123891e8400c382b
nohyunha <nohyunha95@gmail.com>
2024-01-24 22:10:56 +0900

    재고 시스템으로 알아보는 동시성 문제

---
+++ stock/src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,27 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // Stock 조회
+        // 재고를 감소 시킨 뒤
+        // 갱신된 값을 저장하도록 한다.
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+        stock.decrease(quantity);
+
+        stockRepository.save(stock);
+    }
+}
