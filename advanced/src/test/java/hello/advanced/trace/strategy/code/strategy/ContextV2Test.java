c48075a5df1f5d52d79f46ffaa33142578737779
nohyunha <nohyunha95@gmail.com>
2024-02-03 21:16:17 +0900

    전략 패

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2Test.java
@@ -0,0 +1,21 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class ContextV2Test {
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2()); 
+    }
+
+}
