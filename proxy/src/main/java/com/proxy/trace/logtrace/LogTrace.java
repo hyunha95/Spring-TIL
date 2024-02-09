dbf4f0e0edab58fbb4e57979cceafa74d323f5e2
nohyunha <nohyunha95@gmail.com>
2024-02-09 19:26:43 +0900

    proxy 프로젝트 생

---
+++ proxy/src/main/java/com/proxy/trace/logtrace/LogTrace.java
@@ -0,0 +1,10 @@
+package com.proxy.trace.logtrace;
+
+import hello.proxy.trace.TraceStatus;
+
+public interface LogTrace {
+
+    TraceStatus begin(String message);
+    void end(TraceStatus status);
+    void exception(TraceStatus status, Exception e);
+}
